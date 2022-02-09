30f2ec7c9f76e29620d750fa61fde02ef2724e69
Louis-DJPR <louis.cui@ecodev.vic.gov.au>
2022-02-10 01:58:50 +1100

    adding ability for function to read data release date and store it

--- R/read_abs_cabee.R
+++ R/read_abs_cabee.R
@@ -26,6 +26,14 @@ read_abs_cabee <- function(path = tempdir(), delete = TRUE) {
 					   sheet = 3)
 		)
 
+	cabee_emp_raw_time <- cabee_emp_raw %>%
+		dplyr::filter_all(dplyr::any_vars(stringr::str_detect(., pattern = "Released"))) 
+
+	cabee_emp_raw_time <- as.character(cabee_emp_raw_time[ , colSums(is.na(cabee_emp_raw_time)) == 0])
+
+	cabee_emp_raw_date <- sub(".{0,}\\s([0-9]{1,2}\\D+.{3,}\\D+[0-9]{4}).{0,}", "\\1", cabee_emp_raw_time) %>%
+		as.Date(format = "%d %B %Y")
+
 	row_names <- paste(cabee_emp_raw[5,], cabee_emp_raw[6,]) %>%
 		tolower() 
 
@@ -35,10 +43,20 @@ read_abs_cabee <- function(path = tempdir(), delete = TRUE) {
 
 	cabee_emp_raw <- tail(cabee_emp_raw, -6)
 
+	cabee_emp_raw$emp_date <- cabee_emp_raw_date
+
 	cabee_turn_raw <- suppressMessages(
 		readxl::read_excel(file.path(path, file_names[grepl("11.xls", file_names)]),
 					   sheet = 2)
 		)
+
+	cabee_turn_raw_time <- cabee_turn_raw %>%
+		dplyr::filter_all(dplyr::any_vars(stringr::str_detect(., pattern = "Released"))) 
+
+	cabee_turn_raw_time <- as.character(cabee_turn_raw_time[ , colSums(is.na(cabee_turn_raw_time)) == 0])
+
+	cabee_turn_raw_date <- sub(".{0,}\\s([0-9]{1,2}\\D+.{3,}\\D+[0-9]{4}).{0,}", "\\1", cabee_turn_raw_time) %>%
+		as.Date(format = "%d %B %Y")
 
 	row_names <- paste(cabee_turn_raw[5,], cabee_turn_raw[6,]) %>%
 		tolower() 
@@ -49,7 +67,9 @@ read_abs_cabee <- function(path = tempdir(), delete = TRUE) {
 
 	cabee_turn_raw <- tail(cabee_turn_raw, -6)
 
-	df_cabee <- dplyr::left_join(cabee_emp_raw, cabee_turn_raw, by = c("state_name", "lga_code", "lga_label", "industry_code", "industry_label"))
+	cabee_turn_raw$turn_date <- cabee_turn_raw_date
+
+	df_cabee <- dplyr::left_join(cabee_emp_raw, cabee_turn_raw, by = c("state_name", "lga_code", "lga_label", "industry_code", "industry_label", "total_no"))
 
 	if(delete){
 		unlink(file.path(path, file_names))
