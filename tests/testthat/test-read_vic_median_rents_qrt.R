f976bd1d3a76543ff4723574f38d643a5c547fca
Matt Johnson <mrjoh3@gmail.com>
2022-02-10 17:25:34 +1100

    min tests for median rents

--- tests/testthat/test-read_vic_median_rents_qrt.R
+++ tests/testthat/test-read_vic_median_rents_qrt.R
@@ -1,3 +1,34 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+
+test_that("Vic median rents file can be parsed", {
+
+  rent <- read_vic_median_rents_qrt(test = TRUE)
+  columns <- colnames(rent)
+
+  expect_s3_class(rent, 'data.frame')
+  expect_equal(columns, c("district",
+                           "area",
+                           "property",
+                           "date",
+                           "variable",
+                           "value",
+                           "mon",
+                           "mon_num",
+                           "yr"))
+  expect_type(rent$value, 'double')
+  expect_equal(class(rent$date), 'Date')
+
 })
+
+
+test_that('Vic median rents URL has not changed', {
+
+  url <- djprdata:::get_url_vic_median_rents_qrt()
+
+  expect_false(httr::http_error(url$url))
+  expect_false(httr::http_error(url$base_url))
+
+})
+
+
+
