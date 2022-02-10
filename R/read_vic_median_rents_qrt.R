9c37fab1ec4acb49e0dae7b0987c48ad5550b3ac
Matt Johnson <mrjoh3@gmail.com>
2022-02-10 16:06:30 +1100

    add documentation and option to filter data to lga or metro non-metro

--- R/read_vic_median_rents_qrt.R
+++ R/read_vic_median_rents_qrt.R
@@ -1,20 +1,60 @@
 
 
-library(httr)
-library(rvest)
-library(lubridate)
-library(readxl)
-
-#' @title Get median rental prices for Victorian housing
+#' @title Victorian Rental Properties, Count and Median prices.
 #'
-#' @importFrom httr GET
+#' @description Each quarter the Victorian
+#' \link[https://www.dffh.vic.gov.au/]{Department of Families Fairness and Housing (DFFH)}
+#' publish data on the number of rental properties and the median price. Data is included for
+#' each Local Government Area
+#'
+#' @note Replaces the data import component of \url{https://github.com/djpr-data/osd-nous-dashboard/blob/main/processing/rents-by-lga.R}
+#' @import rvest
+#' @import httr
+#' @import readxl
+#' @import dplyr
+#' @importFrom purrr map_dfr
+#' @importFrom tidyr pivot_longer fill
+#' @importFrom assertthat assert_that
 #' @importFrom glue glue
 #'
-#' @return
+#' @return tibble
+#'
+#' @format A data frame with 11 variables. This is an active dataset rows increase weekly:
+#' \describe{
+#'   \item{district \code{character}}{Broad area districts:
+#'     \itemize{
+#'           \item{Barwon South West}
+#'           \item{Grampians}
+#'           \item{Loddon Mallee}
+#'           \item{Hume}
+#'           \item{Gippsland}
+#'           \item{North and West Metro}
+#'           \item{Eastern Metro}
+#'           \item{Southern Metro}
+#'           \item{METRO NON-METRO}
+#'     }
+#'   }
+#'   \item{area \code{character}}{Local Government Area (LGA)}
+#'   \item{property \code{character}}{Property type based on bedroom number}
+#'   \item{date \code{date}}{Record date fields converted from "Year Month" format}
+#'   \item{variable \code{character}}{Type of data}
+#'   \item{value \code{numeric}}{Value}
+#'   \item{mon \code{character}}{Month}
+#'   \item{mon_num \code{character}}{Month number}
+#'   \item{yr \code{numeric}}{Year}
+#' }
+#'
 #' @export
-#' @source https://www.dffh.vic.gov.au/publications/rental-report
+#' @source \url{https://www.dffh.vic.gov.au/publications/rental-report}
 #' @examples
-read_vic_median_rents_qrt <- function(){
+#' \dontrun{
+#'   rents_all <- read_vic_median_rents_qrt()
+#'
+#'   rents_lga <- read_vic_median_rents_qrt(include = "lga")
+#' }
+read_vic_median_rents_qrt <- function(include = c('all','lga','metro'), test = FALSE){
+
+  include <- match.arg(include, several.ok = FALSE)
 
   url <- 'https://www.dffh.vic.gov.au/publications/rental-report'
   url_config <- httr::parse_url(url)
@@ -33,29 +73,38 @@ read_vic_median_rents_qrt <- function(){
   url_config$path <- vic_median_rents_qrt
   latest_url <- build_url(url_config)
 
-
   date_latest <- lubridate::parse_date_time(vic_median_rents_qrt, '%B%Y')
+
+  message(glue::glue('Latest Data From: {format(date_latest, "%B %Y")}'))
 
   # TODO: check here if data already downloaded?
 
-  filename <- tempfile(fileext = '.xlsx')
+  if (test) {
+    filename <- 'data-raw/vic_median_rents_qrt_testing.xlsx'
+  } else {
+    filename <- tempfile(fileext = '.xlsx')
+    resp <- GET(test_url, write_disk(filename, overwrite=TRUE))
+    status <- http_status(resp)
 
-  resp <- GET(test_url, write_disk(filename, overwrite=TRUE))
-  status <- http_status(resp)
-
-  assertthat::assert_that(status$category == "Success",
-                          msg = glue('Download Failed with message: {status$message}'))
-  assertthat::assert_that(http_type(resp) == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                          msg = 'Failed to retrieve a Spreadsheet as Expected')
+    assertthat::assert_that(status$category == "Success",
+                            msg = glue('Download Failed with message: {status$message}'))
+    assertthat::assert_that(http_type(resp) == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                            msg = 'Failed to retrieve a Spreadsheet as Expected')
+  }
 
   sheets <- readxl::excel_sheets(filename)
-
 
   # process and row_bind each sheet
   vic_median_rents <- map_dfr(1:length(sheets), function(i){
 
-    sht <- readxl::read_excel(filename, sheet = sheets[i], skip = 1, col_names = F) |>
-      tidyr::fill(1, .direction = 'down')
+    suppressMessages({
+      sht <- readxl::read_excel(filename,
+                                sheet = sheets[i],
+                                skip = 1,
+                                col_names = FALSE,
+                                na = '-') |>
+        tidyr::fill(1, .direction = 'down')
+    })
 
     rows <- nrow(sht)
 
@@ -64,19 +113,36 @@ read_vic_median_rents_qrt <- function(){
     ## the tidy options were 3-6 times slower than base R
     map_dfr(seq(3, ncol(sht), by = 2), function(n){
 
-      tibble(region = sht[3:rows, 1][[1]],
-             lga = sht[3:rows, 2][[1]],
+      tibble(district = sht[3:rows, 1][[1]],
+             area = sht[3:rows, 2][[1]],
              property = sheets[i],
              date = lubridate::my(as.character(sht[1, n])),
              count = sht[3:rows, n][[1]],
              median = sht[3:rows, n + 1][[1]]
       )
-
     })
+  })
+
+  suppressWarnings({
+
+  out <- vic_median_rents |>
+      filter(across(c(district, area), ~ !grepl('Total', .x))) |>
+      pivot_longer(c('count','median'), names_to = 'variable', values_to = 'value') |>
+      mutate(value = as.numeric(value),
+             mon = month(date, label = TRUE, abbr = TRUE),
+             mon_num = stringr::str_pad(month(date), pad = '0', width = 2, side = 'left'),
+             yr = year(date))
 
   })
 
+  if (include == 'lga') {
+    out <- out |>
+      filter(district != 'METRO NON-METRO')
+  } else if (include == 'metro') {
+    out <- out |>
+      filter(district == 'METRO NON-METRO')
+  }
 
-
+  return(out)
 
 }
