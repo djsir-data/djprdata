52244c459bb00ad4d291f9778df4be07f8cec8cd
Louis-DJPR <louis.cui@ecodev.vic.gov.au>
2022-02-11 16:16:45 +1100

    adding read_ivi() function to read internet vacancy data

---
+++ R/read_ivi.R
@@ -0,0 +1,8 @@
+#' @title Get ABS Weekly Payroll data
+#'
+#' @return
+#' @export
+#' @source https://www.abs.gov.au/statistics/labour/earnings-and-work-hours/weekly-payroll-jobs-and-wages-australia/latest-release
+#' @examples
+
+read_ivi <- function()
