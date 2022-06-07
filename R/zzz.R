4d33cff33f5fc38b28731697bd3a17ffa5118b52
Matt Johnson <mrjoh3@gmail.com>
2022-06-07 17:07:40 +1000

    urls env

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+
+
+urls <- new.env()
+
+with(urls, {
+
+  read_google_mobility <- "https://www.gstatic.com/covid19/mobility/Region_Mobility_Report_CSVs.zip"
+  read_vic_property_prices <- 'https://www.land.vic.gov.au/valuations/resources-and-reports/property-sales-statistics'
+  read_vic_median_rents <- 'https://www.dffh.vic.gov.au/publications/rental-report'
+  read_abs_payroll <- 'https://www.abs.gov.au/statistics/labour/earnings-and-work-hours/weekly-payroll-jobs-and-wages-australia/latest-release'
+  read_abs_cabee <- 'https://www.abs.gov.au/statistics/economy/business-indicators/counts-australian-businesses-including-entries-and-exits/latest-release'
+
+})
