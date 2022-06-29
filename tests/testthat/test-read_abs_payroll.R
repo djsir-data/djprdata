aac774f4f7427e7493e6ca499e10ae9e0b74289a
Annett Finger <annett.finger@ecodev.vic.gov.au>
2022-06-29 15:51:17 +1000

    test-read_abs_payroll done

--- tests/testthat/test-read_abs_payroll.R
+++ tests/testthat/test-read_abs_payroll.R
@@ -1,3 +1,30 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# testing read_abs_payroll function
+
+test_that("ABS Weekly Payroll data works", {
+
+  payroll <- read_abs_payroll()
+
+  expect_s3_class(payroll, 'data.frame')
+  expect_equal(class(payroll$value), 'numeric')
+  expect_equal(class(payroll$date), 'Date')
+
+  expect_equal(colnames(payroll), c("state_name",
+                                  "lga_code",
+                                  "lga_label",
+                                  "industry_code",
+                                  "industry_label",
+                                  "category",
+                                  "value",
+                                  "series",
+                                  "date",
+                                  "release"))
+
+})
+
+test_that('read_abs_payroll URL exists', {
+
+  expect_true(
+    RCurl::url.exists(urls$read_abs_payroll)
+  )
+
 })
