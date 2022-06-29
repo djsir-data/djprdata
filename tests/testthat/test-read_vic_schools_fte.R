55c4688db35faa54088f06bcbcf8cc21f3f12908
Annett Finger <annett.finger@ecodev.vic.gov.au>
2022-06-29 13:38:43 +1000

    first draft - waiting for read_vic_schools_fte to be finished, before writing more tests.

--- tests/testthat/test-read_vic_schools_fte.R
+++ tests/testthat/test-read_vic_schools_fte.R
@@ -1,3 +1,30 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# testing read_vic_schools_fte function
+# there is no URL, urls$read_vic_schools_fte is NULL
+# function not finished yet, will wait for that and then write more tests
+
+test_that("read_vic_schools_fte works", {
+
+  skip_on_ci()
+
+  fte <- read_vic_schools_fte()
+
+  expect_s3_class(fte, 'data.frame')
+# more tests to check class of variables, will have to wait for final product
+#  expect_equal(class(fte$value), 'numeric')
+#  expect_equal(class(fte$date), 'Date')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
