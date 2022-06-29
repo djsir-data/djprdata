9b0255bf2c483403e73516c6ee496471e03ddcd9
Annett Finger <annett.finger@ecodev.vic.gov.au>
2022-06-29 16:54:22 +1000

    test_read_internet_vacancy_report done

--- tests/testthat/test-read_internet_vacancy_report.R
+++ tests/testthat/test-read_internet_vacancy_report.R
@@ -1,3 +1,28 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# testing read_internet_vacancy_report function
+
+test_that("Read internet vacancy report works", {
+
+  ivi <- read_internet_vacancy_report()
+
+  expect_s3_class(iva, 'data.frame')
+  expect_equal(class(ivi$value), 'numeric')
+  expect_equal(class(ivi$observation_date), 'Date')
+
+  expect_equal(colnames(ivi), c("level",
+                                "state",
+                                "region",
+                                "anzsco_code",
+                                "anzsco_title",
+                                "date",
+                                "value",
+                                "obeservation_date"))
+
+})
+
+test_that('read_internet_vacancy_report URL exists', {
+
+  expect_true(
+    RCurl::url.exists(urls$read_internet_vacancy_report)
+  )
+
 })
