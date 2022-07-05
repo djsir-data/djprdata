1a6c36b224e39180d03fc136159de5a41ccc30f5
Annett Finger (DJPR) <annett.finger@ecodev.vic.gov.au>
2022-07-05 17:02:20 +1000

    added 'stringr::'

--- R/read_small_area_labour_markets.R
+++ R/read_small_area_labour_markets.R
@@ -21,7 +21,7 @@ read_salm <- function(url = urls$read_salm, filename = tempfile()) {
   download_link <- djprdata:::get_latest_download_url(url, search_term) |>
     as_tibble() |>
     dplyr::select(url) |>
-    dplyr::filter(str_detect(url, "\\.xlsx$")) |>
+    dplyr::filter(stringr::str_detect(url, "\\.xlsx$")) |>
     mutate(filename = tolower(gsub(".*salm-smoothed-sa2-datafiles-asgs-2016-(.+).*", "\\1", url)))
 
   djprdata:::download_excel(download_link$url, filepath = filename)
