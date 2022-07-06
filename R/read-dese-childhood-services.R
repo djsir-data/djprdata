ff334d2e75d066a6f1e46f9ccb7ecada9d30361d
Annett Finger (DJPR) <annett.finger@ecodev.vic.gov.au>
2022-07-06 12:30:10 +1000

    adding 'stringr::' and 'readr::'

--- R/read-dese-childhood-services.R
+++ R/read-dese-childhood-services.R
@@ -20,7 +20,7 @@ read_dese_childhood_services <- function(url = urls$read_dese_childhood_services
 
   links <- djprdata:::get_latest_download_url(url, search_term) %>%
     as_tibble() %>%
-    dplyr::mutate(date_ref = str_sub(url, start = str_locate(url, 'quarterly-reports/child-care-australia-report-')[,2] + 1),
+    dplyr::mutate(date_ref = stringr::str_sub(url, start = stringr::str_locate(url, 'quarterly-reports/child-care-australia-report-')[,2] + 1),
                   download_link = glue::glue('https://www.dese.gov.au/early-childhood/resources/{date_ref}'))
 
   current_download_url <- djprdata:::get_latest_download_url(links$download_link[1], 'xlsx')
@@ -36,7 +36,7 @@ read_dese_childhood_services <- function(url = urls$read_dese_childhood_services
     filter(!is.na(sa3_code)) %>% # this is the total row
     slice(1:(n()-3)) %>% # last three rows are footnotes
     # Some numbers are presented like "<5" replace these with a value in the middle and round to integer (since it's count)
-    mutate(number_of_services = ifelse(grepl("<", number_of_services), parse_number(number_of_services)/2, number_of_services),
+    mutate(number_of_services = ifelse(grepl("<", number_of_services), readr::parse_number(number_of_services)/2, number_of_services),
            number_of_services = as.numeric(number_of_services) %>% round())
 
   return(v_childhood_services)
