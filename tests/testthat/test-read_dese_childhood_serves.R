d3a46a153d3cfaae26e87a3059ee2c230b05eab5
Annett Finger (DJPR) <annett.finger@ecodev.vic.gov.au>
2022-07-06 12:30:57 +1000

    tests done, but function needs fixing, as first time download consistently fails, second succeeds.

---
+++ tests/testthat/test-read_dese_childhood_serves.R
@@ -0,0 +1,22 @@
+# testing read_dese_childhood_services function
+
+test_that("Read of DESE childhood services Data works", {
+
+  dese <- read_dese_childhood_services()        # fails due to time out. Function needs fixing
+
+  expect_s3_class(dese, 'data.frame')
+  expect_equal(class(dese$number_of_services), 'numeric')
+  expect_equal(class(dese$sa3_code), 'character')
+
+  expect_equal(colnames(dese), c("sa3_code",
+                                 "number_of_services"))
+
+})
+
+test_that('read_dese_childhood_services URL exists', {
+
+  expect_true(
+    RCurl::url.exists(urls$read_dese_childhood_services)        # url exists but test fails
+  )
+
+})
